c97fd6c7196afb6ab5607b50f7bc23fc751014a2
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-09-18 12:01:07 +0300

    majority element

--- Array/majorityElements.ts
+++ Array/majorityElements.ts
@@ -1,25 +1,27 @@
-const numsArray2 = [2, 2, 1, 1, 1, 2, 2]
+const numsArray2 = [3, 2, 3]
 
 function majorityElement(nums: number[]): number {
-  const length = nums.length
+  const majorityElement = nums.length / 2
   const map = new Map()
-  const majority = Math.floor(length / 2)
 
-  for (const num of nums) {
-    const count = map.get(num) || 0
-    map.set(num, count + 1)
+  for (let i = 0; i < nums.length; i++) {
+    // if the number is not in the map, set it to 0
+    const count = map.get(nums[i]) || 0
+    // if the number is in the map, increment the count
+    map.set(nums[i], count + 1)
 
-    if (map.get(num) > majority) {
-      return num
+    if (map.get(nums[i]) > majorityElement) {
+      // if the count is greater than the majority element, return the number
+      return nums[i]
     }
   }
 
   return -1
 }
 
-function majorityElement2(nums: number[]): number {
-  nums.sort((a, b) => a - b)
-  return nums[Math.floor(nums.length / 2)]
-}
+// function majorityElement(nums: number[]): number {
+//   nums.sort((a, b) => a - b)
+//   return nums[Math.floor(nums.length / 2)]
+// }
 
-console.log(majorityElement2(numsArray2))
+console.log(majorityElement(numsArray2))
